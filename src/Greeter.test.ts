1d6aab8bc4dd7a9fad1455dd06bdbb847c833992
aofleejay <aofleejay@hotmail.com>
2020-08-04 13:55:41 +0700

    :sparkles: Morning greeting.

--- src/Greeter.test.ts
+++ src/Greeter.test.ts
@@ -23,3 +23,12 @@ it('greet() capitalize name', () => {
 
   expect(message).toBe('Hello John.')
 })
+
+it('greet() say good morning when the time is 6-12AM', () => {
+  const morningDateTime = new Date('2020-08-04T23:00:00.000Z')
+  const greeter = new Greeter(morningDateTime)
+
+  const message = greeter.greet('john')
+
+  expect(message).toBe('Good morning John.')
+})
--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,11 +1,24 @@
 class Greeter {
+  private date: Date
+  constructor(date: Date = new Date()) {
+    this.date = date
+  }
+
   greet(name: string): string {
     const trimName = name.trim()
     const firstChar = trimName[0].toUpperCase()
     const restChar = trimName.slice(1)
     const formattedName = firstChar + restChar
 
-    return `Hello ${formattedName}.`
+    const hour = this.date.getHours()
+    let greetingMessage = ''
+    if (hour >= 6 && hour <= 12) {
+      greetingMessage = 'Good morning'
+    } else {
+      greetingMessage = 'Hello'
+    }
+
+    return `${greetingMessage} ${formattedName}.`
   }
 }
 
